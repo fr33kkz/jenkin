c85f7f9de6d53cb51b4ac8d1ad22e5042e611f1a
Suraj Chauhan <suraj172singh@gmail.com>
2023-01-16 14:36:19 +0530

    Jenkin file 1st commit

---
+++ file.py
@@ -0,0 +1,61 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 28,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "squares: [4, 16, 36, 64, 100]\n",
+      "cubes: [8, 64, 216, 512, 1000]\n"
+     ]
+    }
+   ],
+   "source": [
+    "nums = [2,4,6,8,10]\n",
+    "sq = [i **2 for i in nums]\n",
+    "cube = [i**3 for i in nums]\n",
+    "print(\"squares:\",sq)\n",
+    "print(\"cubes:\",cube)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
